a37f6507f8e1351f0094861ec6ee89364c522bb4
AamirMahbbob <aamirmahbbob@Ms-MacBook-Pro.local>
2026-06-25 00:49:30 +0500

    allow cors

--- src/app.ts
+++ src/app.ts
@@ -3,9 +3,9 @@
 import express from "express";
 import authRoutes from "./routes/auth.route";
 import ticketRoutes from "./routes/ticket.routes";
-
+import cors from "cors";
 const app = express();
-
+app.use(cors());
 app.use(express.json());
 app.get("/test", (req, res) => res.json({ ok: true }));
 app.use("/api/auth", authRoutes);
